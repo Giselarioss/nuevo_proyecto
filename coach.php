58445b1b3b4d56a69c253ab8eed46fc885d97dab
Gisela Rios <gisee_rios@hotmail.com>
2023-06-01 07:25:19 -0300

    php

--- coach.php
+++ coach.php
@@ -15,17 +15,17 @@ $mensaje = $_POST['mensaje'];
 
 
 // como va a llegar el cuerpo del mail 
-$mensaje = "Este mensaje fue enviado por: ", $nombre, "\r\n";
-$mensaje ,= "Su mail es: ", $mail, "\r\n";
-$mensaje ,= "Telefono de contacto: ", $telefono, "\r\n";
-$mensaje ,= "Genero: ", $genero , "\r\n";
-$mensaje ,= "Experiencia: ", $experienica, "\r\n";
-$mensaje ,= "Pais: ", $pais, "\r\n";
-$mensaje ,= "LinkedIn: ", $url , "\r\n";
-$mensaje ,= "CV: ", $archivo , "\r\n";
-$mensaje ,= " Tema en el que puedo ayudar: ", $problemas, "\r\n";
-$mensaje ,= "Mensaje: ", $_POST['mensaje'], "\r\n";
-$mensaje ,= "Enviado el: ", date('d/m/y', time());
+$mensaje = "Este mensaje fue enviado por: ". $nombre . ",\r\n";
+$mensaje .= "Su mail es: ". $mail . "\r\n";
+$mensaje .= "Telefono de contacto: ". $telefono . "\r\n";
+$mensaje .= "Genero: ". $genero . "\r\n";
+$mensaje .= "Experiencia: ". $experienica . "\r\n";
+$mensaje .= "Pais: ". $pais . "\r\n";
+$mensaje .= "LinkedIn: ". $url . "\r\n";
+$mensaje .= "CV: ". $archivo . "\r\n";
+$mensaje .= " Tema en el que puedo ayudar: ". $problemas. "\r\n";
+$mensaje .= "Mensaje: ". $_POST['mensaje']. "\r\n";
+$mensaje .= "Enviado el: ". date('d/m/Y', time());
 
 $para = 'contacto@vidacoaches.com';
 $asunto = 'Mensaje de Vida coach';
@@ -35,7 +35,7 @@ $asunto = 'Mensaje de Vida coach';
 // 2- el asunto
 // definir el mensaje
 //definir el header
-mail($para,$asunto,utf8_decode($mensaje),$header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
 //Redireccion al haber enviado el form
 header("Location:exito.html");
--- enviar.php
+++ enviar.php
@@ -12,13 +12,13 @@ $mensaje = $_POST['mensaje'];
 
 
 // como va a llegar el cuerpo del mail 
-$mensaje = "Este mensaje fue enviado por: ", $nombre, "\r\n";
-$mensaje ,= "Su mail es: ", $mail, "\r\n";
-$mensaje ,= "Telefono de contacto: ", $telefono, "\r\n";
-$mensaje ,= "Pais: ", $pais, "\r\n";
-$mensaje ,= " Necesita apoyo en: ", $problemas, "\r\n";
-$mensaje ,= "Mensaje: ", $_POST['mensaje'], "\r\n";
-$mensaje ,= "Enviado el: ", date('d/m/y', time());
+$mensaje = "Este mensaje fue enviado por:" . $nombre . ",\r\n";
+$mensaje .= "Su mail es: ". $mail . "\r\n";
+$mensaje .= "Telefono de contacto: ". $telefono . "\r\n";
+$mensaje .= "Pais: ". $pais . "\r\n";
+$mensaje .= " Necesita apoyo en: ". $problemas . "\r\n";
+$mensaje .= "Mensaje: ". $_POST['mensaje'] . "\r\n";
+$mensaje .= "Enviado el: ". date('d/m/Y', time());
 
 $para = 'contacto@vidacoaches.com';
 $asunto = 'Mensaje de Vida coach';
@@ -28,7 +28,7 @@ $asunto = 'Mensaje de Vida coach';
 // 2- el asunto
 // definir el mensaje
 //definir el header
-mail($para,$asunto,utf8_decode($mensaje),$header);
+mail($para, $asunto, utf8_decode($mensaje), $header);
 
 //Redireccion al haber enviado el form
 header("Location:exito.html");
