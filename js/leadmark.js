feb2193f9ddf5d550aab5daa503bd6350bca2c40
Gisela Rios <gisee_rios@hotmail.com>
2023-08-31 14:31:54 -0300

    modific. pagina

--- js/leadmark.js
+++ js/leadmark.js
@@ -64,4 +64,116 @@ document.getElementById('form')
                 alert(JSON.stringify(err));
             });
     });
+    const puntos = document.querySelector('.puntos'); // Obtener el elemento padre
 
+    if (window.innerWidth >= 1200) {
+      let ultimo;
+      for(let i = 1; i <= 3; i++){
+        ultimo = puntos.querySelector(':last-child');
+        puntos.removeChild(ultimo);
+      }
+      
+    } else if (window.innerWidth >= 1024) {
+      let ultimo;
+      for(let i = 1; i <= 2; i++){
+        ultimo = puntos.querySelector(':last-child');
+        puntos.removeChild(ultimo);
+      }
+      
+    } else if (window.innerWidth >= 768) {
+      const ultimo = puntos.querySelector(':last-child');
+      console.log(ultimo);
+      puntos.removeChild(ultimo);
+      
+    } 
+    
+    
+    const grande = document.querySelector('.grande');
+    const punto = document.querySelectorAll('.punto');
+    let posicionState = 0;
+    
+    // Cuando CLICK en punto
+    //   Saber la posición de ese punto
+    // Aplicar un transform translateX al grande
+    // Quitar la clase activo de todos puntos
+    // Añadir la clase activo al punto que hemos hecho click
+    
+    // Recorrer todos los puntos
+    punto.forEach((cadaPunto, i) => {
+    //   Asignamos un click a cada punto
+      punto[i].addEventListener('click', () => {
+        const primerPunto = punto[0];
+        const ultimoPunto = punto[punto.length - 1];
+    //     Guardar la posición de ese punto
+        posicionState = i;
+        dezplazarCarrousel();
+        carrouselBtnPrev.disabled = false;
+        carrouselBtnNext.disabled = false;
+        
+        
+        
+    //     Recorremos todos los puntos 
+        punto.forEach((cadaPunto, i) => {
+          punto[i].classList.remove('active');
+        })
+        
+    //     Añadir clase active en el punto que hemos hecho click
+        punto[i].classList.add('active');
+        if(primerPunto.classList.contains('active')){
+          carrouselBtnPrev.disabled = true;
+        } else if(ultimoPunto.classList.contains('active')) {
+          carrouselBtnNext.disabled = true;
+        }
+        
+      })
+    });
+    
+    // Deplazar carrousel a la derecha o la izquierda
+    const carrouselBtnNext = document.querySelector('.carrousel__btn--next');
+    const carrouselBtnPrev = document.querySelector('.carrousel__btn--prev');
+    
+    
+    carrouselBtnNext.addEventListener('click', () => {
+      const ultimoPunto = punto[punto.length - 1];
+      if(!ultimoPunto.classList.contains('active')) {
+        posicionState++;
+        punto.forEach((cadaPunto, i) => {
+        punto[i].classList.remove('active');
+        })
+      punto[posicionState].classList.add('active');
+        carrouselBtnPrev.disabled = false;
+        dezplazarCarrousel();
+        if(ultimoPunto.classList.contains('active')) {
+          carrouselBtnNext.disabled = true;
+        }
+      } else {
+        carrouselBtnNext.disabled = true;
+      }
+      
+    })
+    
+    carrouselBtnPrev.addEventListener('click', () => {
+      
+      const primerPunto = punto[0];
+      if(!primerPunto.classList.contains('active')) {
+        posicionState--;
+        punto.forEach((cadaPunto, i) => {
+          punto[i].classList.remove('active');
+        })
+        punto[posicionState].classList.add('active');
+        carrouselBtnNext.disabled = false;
+        dezplazarCarrousel();
+        if(primerPunto.classList.contains('active')) {
+          carrouselBtnPrev.disabled = true;
+        }
+      } else {
+        carrouselBtnPrev.disabled = true;
+      }
+    
+      
+    })
+    
+    function dezplazarCarrousel() {
+      let operacion = posicionState * -8.34;
+      grande.style.transform = `translateX(${ operacion }%)`;
+    }
