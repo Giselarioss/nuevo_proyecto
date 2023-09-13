bbdad4300678563b5ff6835db89a949524065fae
Gisela Rios <gisee_rios@hotmail.com>
2023-09-13 14:23:21 -0300

    modal oferta

--- js/leadmark.js
+++ js/leadmark.js
@@ -1,17 +1,19 @@
+$(document).ready(function()
+      {
+         $("#modal").modal("show");
+         
+});
+
 
 // smooth scroll
-$(document).ready(function(){
+$(document).ready(function(){  
     $(".navbar .nav-link").on('click', function(event) {
-
         if (this.hash !== "") {
-
             event.preventDefault();
-
             var hash = this.hash;
-
-            $('html, body').animate({
-                scrollTop: $(hash).offset().top
-            }, 700, function(){
+            $('html, body').animate({              
+                scrollTop: $(hash).offset().top                
+            }, 700, function(){              
                 window.location.hash = hash;
             });
         } 
